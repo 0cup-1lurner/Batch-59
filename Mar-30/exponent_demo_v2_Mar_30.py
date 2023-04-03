a521a2e4b92f89fe98e6bd92da6917b3d02da3c7
Empty-Cup <neelalohitha.k@geeklurn.com>
2023-04-03 17:07:31 +0530

    Add Mar-30-Files

---
+++ Mar-30/exponent_demo_v2_Mar_30.py
@@ -0,0 +1,10 @@
+
+first_num = input()  # int(input())
+second_num = input()
+
+first_num_int = float(first_num)  # first_num = int(first_num) 
+second_num_int = float(second_num)
+
+print(first_num_int, "raised to the power of ", second_num_int, "is ", \
+      first_num_int ** second_num_int)
+
