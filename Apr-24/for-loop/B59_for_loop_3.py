9280d2874a3fcc67b9fd1ffe5e31ae034e17522e
Empty-Cup <neelalohitha.k@geeklurn.com>
2023-04-24 13:22:00 +0530

    Add Apr-24

---
+++ Apr-24/for-loop/B59_for_loop_3.py
@@ -0,0 +1,17 @@
+
+for i in "While Loop":
+    print(i)
+
+print("\nfor loop ends, while loop begins\n")
+
+# Same program using while loop
+phrase = "While Loop"
+index = 0
+
+while index < len(phrase):
+    print(phrase[index])
+    index = index + 1
+    
+
+
+
